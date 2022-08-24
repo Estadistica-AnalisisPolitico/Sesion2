f3f463c961eb0acf4498ad1a247d1e036d36c95b
profemagallanes-unmsm <jmagallanes@pucp.edu.pe>
2022-08-24 17:44:07 -0300

    Update U1E1_code.R

--- U1E1_code.R
+++ U1E1_code.R
@@ -12,8 +12,29 @@ names(inei)[3:5]=gsub(" ","_",betterNames)
 inei=inei[-c(1:4),]
 inei[,3:5]=lapply(inei[,3:5], as.numeric)
 #str(inei)
+
 hip=formula(Porcentaje_de_personas_que_tienen_algun_tipo_de_seguro~.)
 resultado=lm(hip,data=inei[,3:5])
+#interpretar
 summary(resultado)
+#interpretar
 library(lm.beta)
 lm.beta(resultado)
+# hay linealidad
+plot(resultado, 1)
+# hay homoce
+plot(resultado, 3,
+     main = paste0("BPtest?",bptest(resultado)$p.value>0.05))
+# norm residuos
+resSW=shapiro.test(resultado$residuals)
+plot(resultado, 2,
+     main=paste0("SWtest?",resSW$p.value>0.05))
+# multicolinealidad
+library(DescTools)
+VIF(resultado)<5
+#influyentes
+plot(resultado, 5)
+checkReg2=as.data.frame(influence.measures(resultado)$is.inf)
+checkReg2[checkReg2$cook.d & checkReg2$hat,c('cook.d','hat')]
+
+
