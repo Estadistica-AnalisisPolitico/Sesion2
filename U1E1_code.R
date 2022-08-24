5710f891e95b74248e1209e5054ba45a0a9c4440
profemagallanes-unmsm <jmagallanes@pucp.edu.pe>
2022-08-23 23:49:42 -0300

    Update U1E1_code.R

--- U1E1_code.R
+++ U1E1_code.R
@@ -1,3 +1,7 @@
+
+#link="https://github.com/Estadistica-AnalisisPolitico/DataFiles-estadistica/raw/main/U1_e1.xlsx"
+#inei=rio::import(link,skip = 2)
+link="https://github.com/Estadistica-AnalisisPolitico/DataFiles-estadistica/raw/main/U1_e1.csv"
 inei=read.csv(link,skip = 2)
 #names(inei)
 inei=inei[,c("Departamento","Provincia","Demográfico","Hogar","Social")]
@@ -8,10 +12,8 @@ names(inei)[3:5]=gsub(" ","_",betterNames)
 inei=inei[-c(1:4),]
 inei[,3:5]=lapply(inei[,3:5], as.numeric)
 #str(inei)
-dataReg=inei[,3:5]
-#str(dataReg)
 hip=formula(Porcentaje_de_personas_que_tienen_algun_tipo_de_seguro~.)
-resultado=lm(hip,data=dataReg)
+resultado=lm(hip,data=inei[,3:5])
 summary(resultado)
 library(lm.beta)
 lm.beta(resultado)
